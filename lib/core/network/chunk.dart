514a53b815b2a4d3c0f86f498788fff84d7387de
Sylensa <sylensa.adolf@gmail.com>
2024-04-28 13:58:43 +0100

    chuck is for tracking api calls in the application for debuging purposes

---
+++ lib/core/network/chunk.dart
@@ -0,0 +1,5 @@
+
+import 'package:chuck_interceptor/chuck.dart';
+import 'package:coding_challenge/core/helper/helper.dart';
+
+Chuck chuck = Chuck(navigatorKey: rootNavigatorKey, showNotification: false);
