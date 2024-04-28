f15fae4a8fe98d3dfa0977156f3108614390d04b
Sylensa <sylensa.adolf@gmail.com>
2024-04-28 14:18:50 +0100

    response codes

---
+++ lib/core/utils/response_codes.dart
@@ -0,0 +1,9 @@
+class AppResponseCodes {
+  static const String success = 'SUCCESS';
+  static const String failed = 'FAILED';
+  static const String invalidToken = 'INVALID_TOKEN';
+  static const String unauthorized = 'UNAUTHORIZED';
+  static const String invalidCode = 'INVALID_CODE';
+
+
+}
