e137df9beb10f2d61adf11224ffa9da3cd7e0eb0
Sylensa <sylensa.adolf@gmail.com>
2024-04-29 12:37:27 +0100

    made few modifications to the absences model

--- lib/features/absences/model/absences_model.dart
+++ lib/features/absences/model/absences_model.dart
@@ -4,6 +4,8 @@
 
 import 'dart:convert';
 
+import 'package:coding_challenge/core/helper/helper.dart';
+
 AbsencesModel AbsencesModelFromJson(String str) => AbsencesModel.fromJson(json.decode(str));
 
 String AbsencesModelToJson(AbsencesModel data) => json.encode(data.toJson());
@@ -31,15 +33,15 @@ class AbsencesModel {
 class AbsencesPayload {
   int? admitterId;
   String? admitterNote;
-  DateTime? confirmedAt;
-  DateTime? createdAt;
+  String? confirmedAt;
+  String? createdAt;
   int? crewId;
-  DateTime? endDate;
+  String? endDate;
   int? id;
   String? memberNote;
-  DateTime? rejectedAt;
-  DateTime? startDate;
-  Type? type;
+  String? rejectedAt;
+  String? startDate;
+  String? type;
   int? userId;
 
   AbsencesPayload({
@@ -60,52 +62,32 @@ class AbsencesPayload {
   factory AbsencesPayload.fromJson(Map<String, dynamic> json) => AbsencesPayload(
     admitterId: json["admitterId"],
     admitterNote: json["admitterNote"],
-    confirmedAt: json["confirmedAt"] == null ? null : DateTime.parse(json["confirmedAt"]),
-    createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
+    confirmedAt: json["confirmedAt"] == null ? "" : dateFormat(DateTime.parse(json["confirmedAt"])),
+    createdAt: json["createdAt"] == null ? "" : dateFormat(DateTime.parse(json["createdAt"])),
     crewId: json["crewId"],
-    endDate: json["endDate"] == null ? null : DateTime.parse(json["endDate"]),
+    endDate: json["endDate"] == null ? null : dateFormat(DateTime.parse(json["endDate"])),
     id: json["id"],
     memberNote: json["memberNote"],
-    rejectedAt: json["rejectedAt"] == null ? null : DateTime.parse(json["rejectedAt"]),
-    startDate: json["startDate"] == null ? null : DateTime.parse(json["startDate"]),
-    type: typeValues.map[json["type"]]!,
+    rejectedAt: json["rejectedAt"] == null ? "" : dateFormat(DateTime.parse(json["rejectedAt"])),
+    startDate: json["startDate"] == null ? "" : dateFormat(DateTime.parse(json["startDate"])),
+    type: json["type"],
     userId: json["userId"],
   );
 
   Map<String, dynamic> toJson() => {
     "admitterId": admitterId,
     "admitterNote": admitterNote,
-    "confirmedAt": confirmedAt?.toIso8601String(),
-    "createdAt": createdAt?.toIso8601String(),
+    "confirmedAt": confirmedAt,
+    "createdAt": createdAt,
     "crewId": crewId,
-    "endDate": "${endDate!.year.toString().padLeft(4, '0')}-${endDate!.month.toString().padLeft(2, '0')}-${endDate!.day.toString().padLeft(2, '0')}",
+    "endDate": endDate,
     "id": id,
     "memberNote": memberNote,
-    "rejectedAt": rejectedAt?.toIso8601String(),
-    "startDate": "${startDate!.year.toString().padLeft(4, '0')}-${startDate!.month.toString().padLeft(2, '0')}-${startDate!.day.toString().padLeft(2, '0')}",
-    "type": typeValues.reverse[type],
+    "rejectedAt": rejectedAt,
+    "startDate": startDate,
+    "type": type,
     "userId": userId,
   };
 }
 
-enum Type {
-  SICKNESS,
-  VACATION
-}
 
-final typeValues = EnumValues({
-  "sickness": Type.SICKNESS,
-  "vacation": Type.VACATION
-});
-
-class EnumValues<T> {
-  Map<String, T> map;
-  late Map<T, String> reverseMap;
-
-  EnumValues(this.map);
-
-  Map<T, String> get reverse {
-    reverseMap = map.map((k, v) => MapEntry(v, k));
-    return reverseMap;
-  }
-}
