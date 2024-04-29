b9589aaf1dc70b3789ff41bc91e472068008de8c
Sylensa <sylensa.adolf@gmail.com>
2024-04-29 17:24:02 +0100

    implemented search filter and loading state

--- lib/features/absences/view/absences_ui.dart
+++ lib/features/absences/view/absences_ui.dart
@@ -1,10 +1,12 @@
 import 'package:coding_challenge/core/helper/helper.dart';
 import 'package:coding_challenge/core/widget/custom_text_field.dart';
 import 'package:coding_challenge/features/absences/view/widget/absences_list_view_widget.dart';
+import 'package:coding_challenge/features/absences/view/widget/filter_bottom_sheet.dart';
 import 'package:coding_challenge/features/absences/view/widget/smart_refresh.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:coding_challenge/features/absences/controller/absences_controller.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
@@ -27,12 +29,24 @@ class AbsencesPage extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-            child: CustomTextField(
-              placeholder: "Search",
-              onChange: state.onSearchChanged,
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomTextField(
+                    placeholder: "Search",
+                    onChange: state.onSearchChanged,
+                  ),
+                ),
+                IconButton(onPressed: (){
+                  marketPlaceFilter(context);
+                }, icon: Icon(Icons.filter_list_outlined))
+              ],
             ),
           ),
-         Obx(() =>  Expanded(
+         Obx(() =>  state.loading.value ?
+              Expanded(child: Center(child: progressCircular(),)) :
+    state.listAbsences.value.isNotEmpty ?
+         Expanded(
            child: SmartRefresh(
              onLoading: state.onLoading,
              onRefresh: state.onRefresh,
@@ -48,7 +62,13 @@ class AbsencesPage extends StatelessWidget {
            )
 
            ,
-         ))
+         ) :
+         Expanded(child: Center(child: sText( state.errorMessage.value.isEmpty ? "No absences" :  state.errorMessage.value),))),
+        Obx(() =>   Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: sText("Total count: ${state.unFilteredListAbsences.value.length}"),
+        ),)
+
 
         ],
       ),
