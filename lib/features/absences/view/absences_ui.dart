167a597260e3375abaed49013f06eecd069fc301
Sylensa <sylensa.adolf@gmail.com>
2024-04-29 12:38:38 +0100

    refactor the absences main view by building a new component for the pagination using pull to refresh

--- lib/features/absences/view/absences_ui.dart
+++ lib/features/absences/view/absences_ui.dart
@@ -1,5 +1,7 @@
 import 'package:coding_challenge/core/helper/helper.dart';
+import 'package:coding_challenge/core/widget/custom_text_field.dart';
 import 'package:coding_challenge/features/absences/view/widget/absences_list_view_widget.dart';
+import 'package:coding_challenge/features/absences/view/widget/smart_refresh.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:coding_challenge/features/absences/controller/absences_controller.dart';
@@ -23,51 +25,30 @@ class AbsencesPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Obx(() =>  Expanded(
-            child: SmartRefresher(
-      reverse: false,
-      enablePullDown: true,
-      enablePullUp: true,
-      footer: CustomFooter(
-        builder: (BuildContext context,LoadStatus? mode){
-          Widget body ;
-          if(mode==LoadStatus.idle){
-            body =  sText("No more Data");
-          }
-          else if(mode==LoadStatus.loading){
-            body =  const CupertinoActivityIndicator();
-          }
-          else if(mode == LoadStatus.failed){
-            body = sText("Load Failed!Click retry!");
-          }
-          else if(mode == LoadStatus.canLoading){
-            body = sText("release to load more");
-          }
-          else{
-            body = const Text("No more Data");
-          }
-          return SizedBox(
-            height: 55.0,
-            child: Center(child:body),
-          );
-        },
-      ),
-      controller: state.refreshController,
-      onRefresh: state.onRefresh,
-      onLoading: state.onLoading,
-      child: ListView.builder(
-          itemCount: state.listAbsences.value.length,
-          itemBuilder: (BuildContext context, int index){
-            return  Padding(
-              padding: const EdgeInsets.all(20),
-              child: Obx(() => HomeViewWidget(absencesPayload: state.listAbsences.value[index],)),
-            );
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+            child: CustomTextField(
+              placeholder: "Search",
+              onChange: state.onSearchChanged,
+            ),
+          ),
+         Obx(() =>  Expanded(
+           child: SmartRefresh(
+             onLoading: state.onLoading,
+             onRefresh: state.onRefresh,
+             child: ListView.builder(
+                 itemCount: state.listAbsences.value.length,
+                 itemBuilder: (BuildContext context, int index){
+                   return  Padding(
+                     padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                     child: HomeViewWidget(absencesPayload: state.listAbsences.value[index],),
+                   );
 
-          }),
-    )
+                 }),
+           )
 
-            ,
-          ))
+           ,
+         ))
 
         ],
       ),
